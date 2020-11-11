9e2b94974b54c1e2a01fa87b975c77a5ab988d92
alexandrYun <alexandryun@mail.ru>
2020-11-12 01:31:12 +0300

    weather app ready

--- script.js
+++ script.js
@@ -2,19 +2,36 @@ const form = document.querySelector('.city__form');
 const input = document.querySelector('.city__input');
 const submit = document.querySelector('.city__submit');
 const display = document.querySelector('.display');
+const city = document.querySelector('.display__city');
+const temperature = document.querySelector('.display__temperature');
+const wind = document.querySelector('.display__wind');
+const weatherType = document.querySelector('.display__weathertype');
 
 const apiKey = 'e38f7f4aa4ee8e319fc7d462f6f99fda';
-const getRequest = `api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
 
 form.onsubmit = (e) => {
     e.preventDefault();
-    console.log(input.value);
+    submit.style.animation = 'submit 0.2s';
+    setTimeout(() => {
+        submit.style.animation = null;
+    }, 200)
+    let getRequest = `http://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}`;
+    fetch(getRequest)
+    .then(response => response.json())
+    .then(json => {
+        city.innerHTML = json.name;
+        temperature.innerHTML = `${(json.main.temp - 273).toFixed(1)}°`;
+        wind.innerHTML = `Ветер: ${json.wind.speed} м/с`;
+        weatherType.innerHTML = `${json.weather[0].main} (${json.weather[0].description})`;
+        if (json.weather[0].main == 'Clear') {
+            display.classList.remove(...display.classList);
+            display.classList.add('display', 'display_clear');
+        } else if (json.weather[0].main == 'Rain') {
+            display.classList.remove(...display.classList);
+            display.classList.add('display', 'display_rain');
+        } else if (json.weather[0].main == 'Clouds') {
+            display.classList.remove(...display.classList);
+            display.classList.add('display', 'display_cloudy');
+        }
+    })
 }
-
-// if(input.value == '') {
-//     submit.disabled = true;
-//     submit.style.opacity = '0.5';
-// } else if(input.value != '') {
-//     submit.disabled = false;
-// }
-
